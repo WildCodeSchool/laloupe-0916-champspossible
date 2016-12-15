8111a27b2bd1d77c71e3e9458752b6a5285c4c9a
Jipsian <sylvain.labarre@live.fr>
2016-12-15 16:31:16 +0100

    ajout back page d'accueil

---
+++ public/js/controllers/carousselController.js
@@ -0,0 +1,29 @@
+function mainController(homeService) {
+
+    this.homeService = homeService;
+
+    this.load = () => {
+        this.homeService.getAll().then((res) => {
+            this.homes = res.data;
+        });
+    };
+
+    this.create = () => {
+        this.homeService.create(this.home).then(() => {
+            this.home = '';
+            this.load();
+        });
+    };
+
+    this.update = (home) => {
+        this.homeService.update(home._id, home.lienImg, home.lienClick, home.ordre).then(() => {
+            this.load();
+        });
+    };
+
+    this.delete = (home) => {
+        this.homeService.delete(home._id).then(() => {
+            this.load();
+        });
+    };
+  }
