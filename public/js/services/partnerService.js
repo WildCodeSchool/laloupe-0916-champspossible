67145f6c682ddce63fb5207b75c31ca61e63d4c1
LOULAPPE <veronsyl@wanadoo.fr>
2016-12-15 16:30:25 +0100

    add back table partners

---
+++ public/js/services/partnerService.js
@@ -0,0 +1,25 @@
+function partnerService($http) {
+
+    this.$http = $http;
+
+    this.create = (data) => {
+        return this.$http.post('/api/partners/', data )
+    }
+
+    this.getAll = () => {
+        return this.$http.get('/api/partners')
+    }
+
+    this.getOne = (id) => {
+        return this.$http.get('/api/partners/' + id)
+    }
+
+    this.update = (id, data) => {
+        return this.$http.put('/api/partners/' + id, data)
+    }
+
+    this.delete = (id) => {
+        return this.$http.delete('/api/partners/' + id)
+    }
+
+}
