fd59723d143c77978b6268af063db1f022768409
LOULAPPE <veronsyl@wanadoo.fr>
2016-12-16 16:35:35 +0100

    DOM Architecture

---
+++ public/js/controllers/admin/campusController.js
@@ -0,0 +1,29 @@
+function campusController(campusService) {
+
+    this.campusService = campusService;
+
+    this.load = () => {
+        this.campusService.getAll().then((res) => {
+            this.campuss = res.data;
+        });
+    };
+
+    this.create = () => {
+        this.campusService.create(this.campus).then(() => {
+            this.campus = '';
+            this.load();
+        });
+    };
+
+    this.update = (campus) => {
+        this.campusService.update(campus._id, campus.titre, campus.texte, campus.logo, campus.ordre, campus.filtre).then(() => {
+            this.load();
+        });
+    };
+
+    this.delete = (campus) => {
+        this.campusService.delete(campus._id).then(() => {
+            this.load();
+        });
+    };
+}
