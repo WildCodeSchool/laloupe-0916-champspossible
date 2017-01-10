c918a316be62c6279aa9153d71cd271a4d6443e3
LOULAPPE <veronsyl@wanadoo.fr>
2017-01-10 12:22:59 +0100

    page partners evolution

--- public/js/controllers/admin/partnerController.js
+++ public/js/controllers/admin/partnerController.js
@@ -1,12 +1,26 @@
-function partnerController(partnerService) {
+function partnerController(partnerService, $timeout) {
 
     this.partnerService = partnerService;
 
     this.load = () => {
         this.partnerService.getAll().then((res) => {
             this.partners = res.data;
+            $timeout(() => {
+                $('.dropdown-button').dropdown({
+                    inDuration: 300,
+                    outDuration: 225,
+                    constrain_width: false, // Does not change width of dropdown to that of the activator
+                    hover: true, // Activate on hover
+                    gutter: 0, // Spacing from edge
+                    belowOrigin: false, // Displays dropdown below the button
+                    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+                });
+
+                $(".button-collapse").sideNav();
+            }, 0);
         });
     };
+
     this.load();
 
     this.create = () => {
@@ -28,8 +42,9 @@ function partnerController(partnerService) {
                 var urlImage = '/uploads/img_' + document.getElementById('uploadImage-' + index).value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
                 partner.logo = urlImage;
                 this.partnerService.update(partner._id, partner).then(() => {
-                    this.load();
+                    $timeout(() => {this.load();}, 500)
                     // $route.reload();
+
                 });
             };
             fd.append("upload_file", files[i]);
@@ -42,4 +57,6 @@ function partnerController(partnerService) {
             this.load();
         });
     };
+
+
 }
