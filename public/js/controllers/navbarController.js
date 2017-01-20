536e7f1833c76671ccad23c4866c872313f412ba
LOULAPPE <veronsyl@wanadoo.fr>
2017-01-20 10:12:12 +0100

    change user controller and navbar controller

--- public/js/controllers/navbarController.js
+++ public/js/controllers/navbarController.js
@@ -1,31 +1,31 @@
-class navbarController {
+function navbarController(sessionFactory, $rootScope, $window, $location, ephemereService) {
 
-    constructor(sessionFactory, $rootScope, $window, $location, ephemereService) {
-        this.isLogged = sessionFactory.isLogged;
-        this.sessionFactory = sessionFactory;
-        this.$rootScope = $rootScope;
-        this.$location = $location;
-        this.ephemereService = ephemereService;
-        this.load();
-        $rootScope.$on('loginStatusChanged', (event, isLogged) => {
-            this.isLogged = isLogged;
-            this.user = sessionFactory.user;
-        });
-    }
+    this.isLogged = sessionFactory.isLogged;
+    this.sessionFactory = sessionFactory;
+    this.ephemereService = ephemereService;
+    this.$rootScope = $rootScope;
+    this.$location = $location;
 
-    load() {
-        this.ephemereService.getAll().then((res) => {
-            this.ephemeres = res.data;
-        });
-    }
+    $rootScope.$on('loginStatusChanged', (event, isLogged) => {
+        this.isLogged = isLogged;
+        this.user = sessionFactory.user;
+    });
 
-
-    logout() {
+    this.logout = () => {
         this.sessionFactory.isLogged = false;
         this.sessionFactory.user = {};
         this.sessionFactory.token = null;
         this.$rootScope.$emit('loginStatusChanged', false);
         this.isLogged = false;
         this.$location.path('/login');
+    };
+
+    this.load = () => {
+        this.ephemereService.getAll().then((res) => {
+            this.ephemeres = res.data;
+        });
     }
+    
+    this.load();
+
 }
